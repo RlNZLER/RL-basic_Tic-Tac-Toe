20fca670ff2c88a6f0087f7b2252bc6073d1eb83
RlNZLER <amelpvarghese@gmail.com>
2025-08-01 21:05:18 +0100

    Some minor changes

--- src/main.py
+++ src/main.py
@@ -22,6 +22,9 @@ class Tic_Tac_Toe():
         self.window.title('Tic-Tac-Toe')
         self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
         self.canvas.pack()
+        self.play_again_button = Button(self.window, text="Play Again", font=("cmr", 20, "bold"),
+                                bg="lightgray", command=self.on_play_again_click)
+        self.play_again_button.place_forget()
         # Input from user in form of clicks
         self.window.bind('<Button-1>', self.click)
 
@@ -56,6 +59,11 @@ class Tic_Tac_Toe():
         self.player_X_turns = self.player_X_starts
         self.board_status = np.zeros(shape=(3, 3))
 
+    def on_play_again_click(self):
+        self.canvas.delete("all")
+        self.play_again_button.place_forget()
+        self.play_again()
+        self.reset_board = False
     # ------------------------------------------------------------------
     # Drawing Functions:
     # The modules required to draw required game based object on canvas
@@ -83,11 +91,11 @@ class Tic_Tac_Toe():
 
         if self.X_wins:
             self.X_score += 1
-            text = 'Winner: Player 1 (X)'
+            text = 'Winner:\nRL Agent (X)'
             color = symbol_X_color
         elif self.O_wins:
             self.O_score += 1
-            text = 'Winner: Player 2 (O)'
+            text = 'Winner:\nHuman (O)'
             color = symbol_O_color
         else:
             self.tie_score += 1
@@ -97,20 +105,31 @@ class Tic_Tac_Toe():
         self.canvas.delete("all")
         self.canvas.create_text(size_of_board / 2, size_of_board / 3, font="cmr 60 bold", fill=color, text=text)
 
-        score_text = 'Scores \n'
+        # Display Scores title
         self.canvas.create_text(size_of_board / 2, 5 * size_of_board / 8, font="cmr 40 bold", fill=Green_color,
-                                text=score_text)
+                                text='Scores')
 
-        score_text = 'Player 1 (X) : ' + str(self.X_score) + '\n'
-        score_text += 'Player 2 (O): ' + str(self.O_score) + '\n'
-        score_text += 'Tie                    : ' + str(self.tie_score)
-        self.canvas.create_text(size_of_board / 2, 3 * size_of_board / 4, font="cmr 30 bold", fill=Green_color,
-                                text=score_text)
+        # Display score lines with proper vertical spacing
+        start_y = 3 * size_of_board / 4
+        line_spacing = 40  # pixels between lines
+        total_games = self.X_score + self.O_score + self.tie_score
+
+        self.canvas.create_text(size_of_board / 2, start_y, font="cmr 30 bold", fill=Green_color,
+                                text=f'Total Games Played: {total_games}')
+        self.canvas.create_text(size_of_board / 2, start_y + line_spacing, font="cmr 30 bold", fill=Green_color,
+                                text=f'RL Agent (X): {self.X_score}')
+        self.canvas.create_text(size_of_board / 2, start_y + 2 * line_spacing, font="cmr 30 bold", fill=Green_color,
+                                text=f'Human (O)   : {self.O_score}')
+        self.canvas.create_text(size_of_board / 2, start_y + 3 * line_spacing, font="cmr 30 bold", fill=Green_color,
+                                text=f'Ties         : {self.tie_score}')
         self.reset_board = True
 
-        score_text = 'Click to play again \n'
-        self.canvas.create_text(size_of_board / 2, 15 * size_of_board / 16, font="cmr 20 bold", fill="gray",
-                                text=score_text)
+        # score_text = 'Click to play again \n'
+        # self.canvas.create_text(size_of_board / 2, 15 * size_of_board / 16, font="cmr 20 bold", fill="gray",
+        #                         text=score_text)
+
+        # Show "Play Again" button centered at the bottom
+        self.play_again_button.place(relx=0.5, rely=0.5, anchor="center")
 
     # ------------------------------------------------------------------
     # Logical Functions:
